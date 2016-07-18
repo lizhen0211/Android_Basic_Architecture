1164156c12cf5ac4558cdc299cd2c5dcacfd2868
lizhen <lizhen@zhumengyuan.com>
2016-07-18 12:23:22 +0800

    base config

--- app/src/main/java/com/architecture/base/BaseApplication.java
+++ app/src/main/java/com/architecture/base/BaseApplication.java
@@ -1,6 +1,9 @@
 package com.architecture.base;
 
 import android.app.Application;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 
 import com.architecture.biz.dao.DaoMaster;
 import com.architecture.biz.dao.DaoSession;
@@ -18,10 +21,25 @@ public class BaseApplication extends Application {
 
     private DaoSession daoSession;
 
+    public static String packageName;
+
+    public static String versionName;
+
+    public static int versionCode;
+
+    public static String channelName;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        initConfig();
+        initDataBase();
+    }
 
+    /**
+     * 初始化数据库
+     */
+    private void initDataBase() {
         DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, ENCRYPTED ? "notes-db-encrypted" : "notes-db");
         Database db = ENCRYPTED ? helper.getEncryptedWritableDb("super-secret") : helper.getWritableDb();
         daoSession = new DaoMaster(db).newSession();
@@ -30,4 +48,27 @@ public class BaseApplication extends Application {
     public DaoSession getDaoSession() {
         return daoSession;
     }
+
+    public void initConfig() {
+        packageName = getPackageName();
+
+        PackageManager packageManager = getPackageManager();
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = packageManager.getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        versionName = packageInfo.versionName;
+        versionCode = packageInfo.versionCode;
+
+        ApplicationInfo applicationInfo = null;
+        try {
+            applicationInfo = packageManager.getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        channelName = applicationInfo.metaData.getString("CHANNEL_KEY");
+    }
+
 }
